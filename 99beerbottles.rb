02a7fbe98df0c7c091146625780c9b76c44d16ee
ajay mathrani <ajaymathrani@gmail.com>
2014-03-04 19:22:46 +0530

    recursive 99beerbottles

--- 99beerbottles.rb
+++ 99beerbottles.rb
@@ -5,13 +5,22 @@ while n > 2
 	puts ''
 	n = n-1
 end
+
 if n == 2
 	puts n.to_s + ' bottles of beer on the wall, ' + n.to_s + ' bottles of beer.'
 	puts 'Take one down, pass it around, ' + (n-1).to_s + ' bottle of beer on the wall...'
 	puts ''
 	n = n-1
 end
+
 if n == 1
+	puts '1 bottle of beer on the wall, 1 bottle of beer.'
+	puts 'Take one down and pass in around, no more bottles of beer on the wall...'	
+	puts ''
+	n = n - 1
+end
+
+if n == 0
 	puts 'No more bottles of beer on the wall, no more bottles of beer'
 	puts 'Go to the store and buy some more, 99 bottles of beer on the wall...'	
 	puts ''
